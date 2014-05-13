9f13c602b15dd44b97bd66bbbf9f5135f4717cc5
Stephen Griffin <stephenegriffin@users.noreply.github.com>
2014-05-13 02:58:48 +0000

    Updated to appforoffice api v 1.1

--- MessageHeaderAnalyzerWeb/Scripts/_references.js
+++ MessageHeaderAnalyzerWeb/Scripts/_references.js
@@ -1,4 +1,12 @@
-﻿/// <reference path="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.2.js" />
+﻿/* Required to correctly initalize Office.js for intellisense */
+/// <reference path="_officeintellisense.js" />
+/* Use offline copy of Office.js for intellisense */
+/// <reference path="office/1.1/outlook-win32.debug.js" />
+/// <reference path="office/1.1/office.js" />
+/* Use online copy of Office.js for intellisense */
+// /// <reference path="https://appsforoffice.microsoft.com/lib/1.1/hosted/outlook-win32.debug.js" />
+// /// <reference path="https://appsforoffice.microsoft.com/lib/1.1/hosted/office.js" />
+/// <reference path="jquery-2.1.0.js" />
 /// <reference path="siteTypesOffice.js" />
 /// <reference path="https://az88874.vo.msecnd.net/api/1.0/outlook-15.debug.js" />
 /// <reference path="https://az88874.vo.msecnd.net/api/1.0/office.js" />
