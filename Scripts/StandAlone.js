25d0d2df490a42fba942e641925fffecdd785689
Stephen Griffin <sgriffin@microsoft.com>
2017-09-30 11:52:33 -0400

    Get Standalone working again

--- Scripts/StandAlone.js
+++ Scripts/StandAlone.js
@@ -15,9 +15,12 @@ function analyzeHeaders() {
     setArrows(viewModel.receivedHeaders.tableName, "hop", 1);
     setArrows(viewModel.otherHeaders.tableName, "number", 1);
 
+    enableSpinner();
     updateStatus(ImportedStrings.mha_loading);
 
     parseHeadersToTables($("#inputHeaders").val());
+
+    disableSpinner();
 }
 
 function clearHeaders() {
@@ -28,4 +31,24 @@ function clearHeaders() {
     setArrows(viewModel.otherHeaders.tableName, "number", 1);
     rebuildSections();
     recalculateLayout();
+}
+
+function enableSpinner() {
+    $("#response").css("background-image", "url(../Resources/loader.gif)");
+    $("#response").css("background-repeat", "no-repeat");
+    $("#response").css("background-position", "center");
+}
+
+function disableSpinner() {
+    $("#response").css("background", "none");
+}
+
+function updateStatus(statusText) {
+    $("#status").text(statusText);
+    if (viewModel !== null) {
+        viewModel.status = statusText;
+    }
+
+    positionResponse();
+    recalculateVisibility();
 }
