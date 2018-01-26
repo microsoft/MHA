640c4aa86d3b4c8552cb29a0ab8b4558076854e4
Stephen Griffin <sgriffin@outlook.com>
2018-01-25 20:39:31 -0500

    Add some diagnostics to the mobile diff page

--- Scripts/MobilePane.js
+++ Scripts/MobilePane.js
@@ -10,4 +10,26 @@ $(document).ready(function () {
     } else {
         $('#message').text('YOU SHOULD NOT BE HERE');
     }
+
+    insertData('diag', 'User agent', window.navigator.userAgent);
+    insertData('diag', 'iOS', Framework7.prototype.device.ios);
+    insertData('diag', 'iPad', Framework7.prototype.device.ipad);
+    insertData('diag', 'iPhone', Framework7.prototype.device.iphone);
+    insertData('diag', 'Android', Framework7.prototype.device.android);
 });
+
+function insertData(id, headerText, valueText) {
+    var pane = $("#" + id);
+
+    var lf = $(document.createElement("br"));
+
+    var header = $(document.createElement("span"));
+    header.text(headerText + ': ');
+
+    var value = $(document.createElement("span"));
+    value.text(valueText);
+
+    pane.append(header);
+    pane.append(value);
+    pane.append(lf);
+}
