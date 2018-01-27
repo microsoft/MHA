3c6334ae09d9d1cf09e0661f92b807ab6aeac9b2
Stephen Griffin <sgriffin@outlook.com>
2018-01-27 18:43:35 -0500

    Defer 2047 decoding until after line unfolding so we don't introduce extraneous whitespace.

--- Scripts/Headers.js
+++ Scripts/Headers.js
@@ -52,44 +52,7 @@ Header.prototype.value = "";
 HeaderModel.prototype.parseHeaders = function (headers) {
     // Initialize originalHeaders in case we have parsing problems
     this.originalHeaders = headers;
-    var lines = GetClean2047Lines(headers);
-
-    var headerList = [];
-    var iNextHeader = 0;
-    for (var iLine = 0; iLine < lines.length; iLine++) {
-        // Before processing a line, remove any nulls so we don't have display problems.
-        var line = lines[iLine].replace(/\0/g, "");
-
-        // Recognizing a header:
-        // - First colon comes before first white space.
-        // - We're not strictly honoring white space folding because initial white space.
-        // - is commonly lost. Instead, we heuristically assume that space before colon must have been folded.
-        // This expression will give us:
-        // match[1] - everything before the first colon, assuming no spaces (header).
-        // match[2] - everything after the first colon (value).
-        var match = line.match(/(^[\w-\.]*?): ?(.*)/);
-
-        // There's one false positive we might get: if the time in a Received header has been
-        // folded to the next line, the line might start with something like "16:20:05 -0400".
-        // This matches our regular expression. The RFC does not preclude such a header, but I've
-        // never seen one in practice, so we check for and exclude 'headers' that
-        // consist only of 1 or 2 digits.
-        if (match && match[1] && !match[1].match(/^\d{1,2}$/)) {
-            headerList[iNextHeader] = new Header(match[1], match[2]);
-            iNextHeader++;
-        } else {
-            if (iNextHeader > 0) {
-                // Tack this line to the previous line
-                headerList[iNextHeader - 1].value += " " + line;
-            } else {
-                // If we didn't have a previous line, go ahead and use this line
-                if (line.match(/\S/g)) {
-                    headerList[iNextHeader] = new Header("", line);
-                    iNextHeader++;
-                }
-            }
-        }
-    }
+    var headerList = GetHeaderList(headers);
 
     if (headerList.length > 0) {
         this.hasData = true;
@@ -119,18 +82,59 @@ HeaderModel.prototype.parseHeaders = function (headers) {
     this.receivedHeaders.computeDeltas();
 };
 
-function GetClean2047Lines(headers) {
+function GetHeaderList(headers) {
     // First, break up out input by lines.
-    var lines = headers.match(/^.*([\n\r]+|$)/gm);
-    var linesOut = [];
+    var lines = headers.split(/[\n\r]+/);
 
-    // Next, for each line, clean out the 2047 encoding.
-    // This may produce multiple lines as carriage returns may be embedded. Break those up as well before adding to the final list.
+    var headerList = [];
+    var iNextHeader = 0;
+    // Unfold lines
     for (var iLine = 0; iLine < lines.length; iLine++) {
-        linesOut = linesOut.concat(clean2047Encoding(lines[iLine]).match(/^.*([\n\r]+|$)/gm));
+        var line = lines[iLine];
+        // Skip empty lines
+        if (line === "") continue;
+
+        // Recognizing a header:
+        // - First colon comes before first white space.
+        // - We're not strictly honoring white space folding because initial white space
+        // - is commonly lost. Instead, we heuristically assume that space before a colon must have been folded.
+        // This expression will give us:
+        // match[1] - everything before the first colon, assuming no spaces (header).
+        // match[2] - everything after the first colon (value).
+        var match = line.match(/(^[\w-\.]*?): ?(.*)/);
+
+        // There's one false positive we might get: if the time in a Received header has been
+        // folded to the next line, the line might start with something like "16:20:05 -0400".
+        // This matches our regular expression. The RFC does not preclude such a header, but I've
+        // never seen one in practice, so we check for and exclude 'headers' that
+        // consist only of 1 or 2 digits.
+        if (match && match[1] && !match[1].match(/^\d{1,2}$/)) {
+            headerList[iNextHeader] = new Header(match[1], match[2]);
+            iNextHeader++;
+        } else {
+            if (iNextHeader > 0) {
+                // Tack this line to the previous line
+                headerList[iNextHeader - 1].value += " " + line;
+            } else {
+                // If we didn't have a previous line, go ahead and use this line
+                if (line.match(/\S/g)) {
+                    headerList[iNextHeader] = new Header("", line);
+                    iNextHeader++;
+                }
+            }
+        }
     }
 
-    return linesOut;
+    // 2047 decode our headers now
+    for (var iHeader = 0; iHeader < headerList.length; iHeader++) {
+        // Clean 2047 encoding
+        // Strip nulls
+        // Strip trailing carriage returns
+        var headerValue = clean2047Encoding(headerList[iHeader].value).replace(/\0/g, "").replace(/[\n\r]+$/, "");
+        headerList[iHeader].value = headerValue;
+    }
+
+    return headerList;
 }
 
 function mapHeaderToURL(headerName, text) {
