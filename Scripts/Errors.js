2b95c53eb659adab86d547e0e47a81931604da7c
Stephen Griffin <sgriffin@outlook.com>
2018-04-14 18:59:31 -0400

    rebuild LogError to parseError with a callback

--- Scripts/Errors.js
+++ Scripts/Errors.js
@@ -2,10 +2,13 @@
 /* global StackTrace */
 /* exported LogError */
 
+// TODO: split out as much as possible into unit testable functions
+// uiToggle will compose these functions without unit testing
+
 // error - an exception object
 // message - a string describing the error
-// suppressTracking - boolean indicating if we should suppress tracking
-function LogError(exception, message, suppressTracking) {
+// errorHandler - function to call with parsed error
+function parseError(exception, message, errorHandler) {
     var stack;
     var exceptionMessage = exception ? (exception.message ? exception.message : exception.description) : '';
     var eventName = joinArray([message, exceptionMessage], ' : ');
@@ -17,24 +20,33 @@ function LogError(exception, message, suppressTracking) {
         stack = FilterStack(stackframes).map(function (sf) {
             return sf.toString();
         });
-        pushError(eventName, stack, suppressTracking);
+        errorHandler(eventName, stack);
     };
 
     var errback = function (err) {
         stack = [exception.stack, "Parsing error:", err.message, err.stack];
-        pushError(eventName, stack, suppressTracking);
+        errorHandler(eventName, stack);
     };
 
     if (!exception || Object.prototype.toString.call(exception) === "[object String]") {
-        pushError(JSON.stringify(exception), null, suppressTracking);
+        errorHandler(JSON.stringify(exception), null);
         StackTrace.get().then(callback).catch(errback);
     } else {
         StackTrace.fromError(exception).then(callback).catch(errback);
-
-        if (!suppressTracking) {
-            appInsights.trackException(exception);
-        }
     }
+}
+
+// error - an exception object
+// message - a string describing the error
+// suppressTracking - boolean indicating if we should suppress tracking
+function LogError(exception, message, suppressTracking) {
+    if (!suppressTracking && exception && Object.prototype.toString.call(exception) !== "[object String]") {
+        appInsights.trackException(exception);
+    }
+
+    parseError(exception, message, function (eventName, stack) {
+        pushError(eventName, stack, suppressTracking);
+    });
 }
 
 // Join an array with char, dropping empty/missing entries
