bc8fc7202b442434b53b18f4f3fc0131fe3534dc
Stephen Griffin <sgriffin@microsoft.com>
2018-04-17 16:35:25 -0400

    Relax permission check for EWS

--- Scripts/GetHeaders.js
+++ Scripts/GetHeaders.js
@@ -16,7 +16,7 @@ function sendHeadersRequest(headersLoadedCallback) {
         return;
     }
 
-    if (!sufficientPermission()) {
+    if (!sufficientPermission(false)) {
         ShowError(null, "Insufficient permissions to request headers", false);
         return;
     }
@@ -33,16 +33,19 @@ function sendHeadersRequest(headersLoadedCallback) {
     }
 }
 
-function sufficientPermission() {
+function sufficientPermission(strict) {
     if (!Office.context.mailbox) return false;
-    if (!Office.context.mailbox._initialData$p$0) return false;
+    // In strict mode, we must find permissions to conclude we have them
+    // In non-strict mode, if we don't find permissions, we assume we might have them
+    // Some down level clients (such as we would use EWS on) don't have _initialData$p$0 at all.
+    if (!Office.context.mailbox._initialData$p$0) return !strict;
     if (Office.context.mailbox._initialData$p$0._permissionLevel$p$0 < 1) return false;
     return true;
 }
 
 function canUseRest() {
     if (!Office.context.requirements.isSetSupported("Mailbox", 1.5)) return false;
-    if (!sufficientPermission()) return false;
+    if (!sufficientPermission(true)) return false;
     if (!Office.context.mailbox.getCallbackTokenAsync) return false;
     return true;
 }
