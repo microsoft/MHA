69de92c0613501f0e759414ee59cedb216b3c16d
Stephen Griffin <sgriffin@microsoft.com>
2019-11-05 14:58:24 -0500

    Add unittest files to minification

--- tasks/minify.js
+++ tasks/minify.js
@@ -4,10 +4,16 @@ const path = require("path");
 
 const scriptsFolder = path.join(__dirname, "..", "Scripts");
 const distFolder = path.join(__dirname, "..", "dist");
+const unittestsFolder = path.join(__dirname, "..", "dist", "unittests");
 
-// Ensure dist directory exists
+
+// Ensure directories exist
 if (!fs.existsSync(distFolder)) {
     fs.mkdirSync(distFolder);
+}
+
+if (!fs.existsSync(unittestsFolder)) {
+    fs.mkdirSync(unittestsFolder);
 }
 
 var options = {
@@ -36,7 +42,13 @@ const targets = {
     "Strings.min.js": [ "Strings.js" ],
     "Summary.min.js": [ "Summary.js" ],
     "Table.min.js": [ "Table.js" ],
-    "uiToggle.min.js": [ "uiToggle.js" ]
+    "uiToggle.min.js": [ "uiToggle.js" ],
+    "unittests/ut-2047.min.js": ["unittests/ut-2047.js"],
+    "unittests/ut-DateTime.min.js": ["unittests/ut-DateTime.js"],
+    "unittests/ut-GetHeaderList.min.js": ["unittests/ut-GetHeaderList.js"],
+    "unittests/ut-Received.min.js": ["unittests/ut-Received.js"],
+    "unittests/ut-ParseError.min.js": ["unittests/ut-ParseError.js"],
+    "unittests/ut-Xml.min.js": ["unittests/ut-Xml.js"]
 };
 
 for (const targetName of Object.keys(targets)) {
