34d5beb6980fba9cae6b129d8c4dad5b5946cc48
Stephen Griffin <sgriffin@microsoft.com>
2019-11-06 15:33:22 -0500

    First test building map files

--- tasks/minify.js
+++ tasks/minify.js
@@ -18,31 +18,32 @@ if (!fs.existsSync(unittestsFolder)) {
 
 var options = {
     compress: {},
-    mangle: {}
+    mangle: {},
+    sourceMap: {}
 };
 
 const targets = {
-    "2047.min.js": [ "2047.js" ],
-    "Antispam.min.js": [ "Antispam.js" ],
-    "Default.min.js": [ "Default.js" ],
-    "DesktopPane.min.js": [ "DesktopPane.js" ],
-    "Errors.min.js": [ "Errors.js" ],
-    "ForefrontAntispam.min.js": [ "ForefrontAntispam.js" ],
-    "Functions.min.js": [ "Functions.js" ],
-    "GetHeaders.min.js": [ "GetHeaders.js" ],
-    "GetHeadersEWS.min.js": [ "GetHeadersEWS.js" ],
-    "GetHeadersRest.min.js": [ "GetHeadersRest.js" ],
-    "Headers.min.js": [ "Headers.js" ],
-    "MobilePane-ios.min.js": [ "MobilePane-ios.js" ],
-    "MobilePane.min.js": [ "MobilePane.js" ],
-    "Other.min.js": [ "Other.js" ],
-    "Received.min.js": [ "Received.js" ],
-    "siteTypesOffice.min.js": [ "siteTypesOffice.js" ],
-    "StandAlone.min.js": [ "StandAlone.js" ],
-    "Strings.min.js": [ "Strings.js" ],
-    "Summary.min.js": [ "Summary.js" ],
-    "Table.min.js": [ "Table.js" ],
-    "uiToggle.min.js": [ "uiToggle.js" ],
+    "2047.min.js": ["2047.js"],
+    "Antispam.min.js": ["Antispam.js"],
+    "Default.min.js": ["Default.js"],
+    "DesktopPane.min.js": ["DesktopPane.js"],
+    "Errors.min.js": ["Errors.js"],
+    "ForefrontAntispam.min.js": ["ForefrontAntispam.js"],
+    "Functions.min.js": ["Functions.js"],
+    "GetHeaders.min.js": ["GetHeaders.js"],
+    "GetHeadersEWS.min.js": ["GetHeadersEWS.js"],
+    "GetHeadersRest.min.js": ["GetHeadersRest.js"],
+    "Headers.min.js": ["Headers.js"],
+    "MobilePane-ios.min.js": ["MobilePane-ios.js"],
+    "MobilePane.min.js": ["MobilePane.js"],
+    "Other.min.js": ["Other.js"],
+    "Received.min.js": ["Received.js"],
+    "siteTypesOffice.min.js": ["siteTypesOffice.js"],
+    "StandAlone.min.js": ["StandAlone.js"],
+    "Strings.min.js": ["Strings.js"],
+    "Summary.min.js": ["Summary.js"],
+    "Table.min.js": ["Table.js"],
+    "uiToggle.min.js": ["uiToggle.js"],
     "unittests/ut-2047.min.js": ["unittests/ut-2047.js"],
     "unittests/ut-DateTime.min.js": ["unittests/ut-DateTime.js"],
     "unittests/ut-GetHeaderList.min.js": ["unittests/ut-GetHeaderList.js"],
@@ -52,8 +53,9 @@ const targets = {
 };
 
 for (const targetName of Object.keys(targets)) {
-
     const fileSet = targets[targetName];
+    const mapName = targetName + ".map";
+    console.log("Building " + targetName + " and " + mapName);
 
     // Create the list of files and read their sources
     const files = {};
@@ -61,5 +63,9 @@ for (const targetName of Object.keys(targets)) {
         files[file] = fs.readFileSync(path.join(scriptsFolder, file), "utf8");
     }
 
-    fs.writeFileSync(path.join(distFolder, targetName), UglifyJS.minify(files, options).code, "utf8");
+    options.sourceMap.filename = targetName;
+    options.sourceMap.url = mapName;
+    var result = UglifyJS.minify(files, options);
+    fs.writeFileSync(path.join(distFolder, targetName), result.code, "utf8");
+    fs.writeFileSync(path.join(distFolder, mapName), result.map, "utf8");
 }
