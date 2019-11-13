82b75044169ea4cc4c751e58c198c1813fc7bbaa
Stephen Griffin <sgriffin@microsoft.com>
2019-11-13 17:51:44 -0500

    Move app insights script block into diag.js

--- Scripts/diag.js
+++ Scripts/diag.js
@@ -1,6 +1,5 @@
 /* global $ */
 /* exported ensureDiag */
-/* exported telemetryInitializer */
 /* exported getDiagnosticsMap */
 /* exported setItemDiagnostics */
 /* exported clearItemDiagnostics */
@@ -32,30 +31,7 @@ function ensureLastModified() {
 }
 
 function ensureDiagnostics() {
-    mhadiagnostics = mhadiagnostics || new DiagModel;
-}
-
-function telemetryInitializer(envelope) {
-    envelope.data.baseType = envelope.baseType;
-    envelope.data.baseData = envelope.baseData;
-    // This will get called for any appInsights tracking - we can augment or suppress logging from here
-    // No appInsights logging for localhost/dev
-    if (document.domain == "localhost") return false;
-    if (envelope.baseType == "RemoteDependencyData") return true;
-    if (envelope.baseType == "PageviewData") return true;
-    if (envelope.baseType == "PageviewPerformanceData") return true;
-
-    // If we're not one of the above types, tag in our diagnostics data
-    if (envelope.baseType == "ExceptionData") {
-        // custom data for the ExceptionData type lives in a different place
-        envelope.baseData.properties = envelope.baseData.properties || {};
-        $.extend(envelope.baseData.properties, getDiagnosticsMap());
-    }
-    else {
-        $.extend(envelope.data, getDiagnosticsMap());
-    }
-
-    return true;
+    mhadiagnostics = mhadiagnostics || new DiagModel();
 }
 
 // Combines mhadiagnostics.appDiagnostics and mhadiagnostics.itemDiagnostics and returns a single object
@@ -197,3 +173,33 @@ function getRequirementSet() {
         return "Could not detect requirements set";
     }
 }
+
+// app Insights initialization
+var sdkInstance = "appInsightsSDK"; window[sdkInstance] = "appInsights"; var aiName = window[sdkInstance], aisdk = window[aiName] || function (e) { function n(e) { t[e] = function () { var n = arguments; t.queue.push(function () { t[e].apply(t, n) }) } } var t = { config: e }; t.initialize = !0; var i = document, a = window; setTimeout(function () { var n = i.createElement("script"); n.src = e.url || "https://az416426.vo.msecnd.net/scripts/b/ai.2.min.js", i.getElementsByTagName("script")[0].parentNode.appendChild(n) }); try { t.cookie = i.cookie } catch (e) { } t.queue = [], t.version = 2; for (var r = ["Event", "PageView", "Exception", "Trace", "DependencyData", "Metric", "PageViewPerformance"]; r.length;)n("track" + r.pop()); n("startTrackPage"), n("stopTrackPage"); var s = "Track" + r[0]; if (n("start" + s), n("stop" + s), n("setAuthenticatedUserContext"), n("clearAuthenticatedUserContext"), n("flush"), !(!0 === e.disableExceptionTracking || e.extensionConfig && e.extensionConfig.ApplicationInsightsAnalytics && !0 === e.extensionConfig.ApplicationInsightsAnalytics.disableExceptionTracking)) { n("_" + (r = "onerror")); var o = a[r]; a[r] = function (e, n, i, a, s) { var c = o && o(e, n, i, a, s); return !0 !== c && t["_" + r]({ message: e, url: n, lineNumber: i, columnNumber: a, error: s }), c }, e.autoExceptionInstrumented = !0 } return t }(
+    {
+        instrumentationKey: "2f12afed-6139-456e-9de3-49003d3a1fb1"
+    }
+); window[aiName] = aisdk, ensureDiagnostics(), aisdk.queue.push(function () {
+    aisdk.addTelemetryInitializer(function (envelope) {
+        envelope.data.baseType = envelope.baseType;
+        envelope.data.baseData = envelope.baseData;
+        // This will get called for any appInsights tracking - we can augment or suppress logging from here
+        // No appInsights logging for localhost/dev
+        if (document.domain == "localhost") return false;
+        if (envelope.baseType == "RemoteDependencyData") return true;
+        if (envelope.baseType == "PageviewData") return true;
+        if (envelope.baseType == "PageviewPerformanceData") return true;
+
+        // If we're not one of the above types, tag in our diagnostics data
+        if (envelope.baseType == "ExceptionData") {
+            // custom data for the ExceptionData type lives in a different place
+            envelope.baseData.properties = envelope.baseData.properties || {};
+            $.extend(envelope.baseData.properties, getDiagnosticsMap());
+        }
+        else {
+            $.extend(envelope.data, getDiagnosticsMap());
+        }
+
+        return true;
+    });
+}), aisdk.queue && 0 === aisdk.queue.length && aisdk.trackPageView({});
