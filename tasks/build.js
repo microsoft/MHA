61f94d5019d34022568af4ad9788bd6b789e5234
Stephen Griffin <sgriffin@outlook.com>
2019-12-02 22:59:22 -0500

    merge build scripts

--- tasks/build.js
+++ tasks/build.js
@@ -1,8 +1,22 @@
 const UglifyJS = require("uglify-js");
 const fs = require("fs");
 const path = require("path");
+const process = require("process");
 
 const scriptsFolder = path.join(__dirname, "..", "Scripts");
+
+console.log("key found in env: " + process.env.APPINSIGHTS_INSTRUMENTATIONKEY);
+const key = process.env.APPINSIGHTS_INSTRUMENTATIONKEY ? process.env.APPINSIGHTS_INSTRUMENTATIONKEY : "2f12afed-6139-456e-9de3-49003d3a1fb1";
+const aiscript = path.join(scriptsFolder, "aikey.js");
+
+console.log("Merging AppInsights key (" + key + ") into js");
+if (fs.existsSync(aiscript)) {
+    console.log("  Deleting " + aiscript);
+    fs.unlinkSync(aiscript);
+}
+
+console.log("Building " + aiscript);
+fs.writeFileSync(aiscript, "/* exported aikey */ window.aikey = function () { return \"" + key + "\"; };", "utf8");
 
 var options = {
     compress: {},
