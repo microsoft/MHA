c7812127869daa6bc76783a7e7a623b0cec54cab
Stephen Griffin <sgriffin@outlook.com>
2019-12-07 20:43:03 -0500

    move url map to strings

--- Scripts/Strings.js
+++ Scripts/Strings.js
@@ -1,4 +1,5 @@
 /* exported ImportedStrings */
+/* exported mapHeaderToURL */
 
 var ImportedStrings = {
     // REST
@@ -67,3 +68,71 @@ var ImportedStrings = {
     // AntiSpamReport
     mha_bcl: "Bulk Complaint Level"
 };
+
+function htmlEncode(value) { return value ? $('<div />').text(value).html() : ''; }
+
+function mapHeaderToURL(headerName, text) {
+    for (var i = 0; i < HeaderToURLMap.length; i++) {
+        if (headerName.toLowerCase() === HeaderToURLMap[i][0].toLowerCase()) {
+            return ["<a href = '", HeaderToURLMap[i][1], "' target = '_blank'>", htmlEncode(text || headerName), "</a>"].join("");
+        }
+    }
+
+    return null;
+}
+
+var HeaderToURLMap = [
+    ["Accept-Language", "https://tools.ietf.org/html/rfc3282"],
+    ["Authentication-Results", "https://tools.ietf.org/html/rfc7601"],
+    ["BCC", "https://tools.ietf.org/html/rfc5322#section-3.6.3"],
+    ["CC", "https://tools.ietf.org/html/rfc5322#section-3.6.3"],
+    ["Content-Description", "https://tools.ietf.org/html/rfc2045#section-8"],
+    ["Content-Disposition", "https://tools.ietf.org/html/rfc2183"],
+    ["Content-Id", "https://tools.ietf.org/html/rfc2045#section-7"],
+    ["Content-Language", "https://tools.ietf.org/html/rfc3282"],
+    ["Content-Transfer-Encoding", "https://tools.ietf.org/html/rfc2045#section-6"],
+    ["Content-Type", "https://tools.ietf.org/html/rfc2045#section-5"],
+    ["Date", "https://tools.ietf.org/html/rfc5322#section-3.6.1"],
+    ["Deferred-Delivery", "https://tools.ietf.org/html/rfc4021#section-2.1.65"],
+    ["DKIM-Signature", "https://tools.ietf.org/html/rfc6376"],
+    ["From", "https://tools.ietf.org/html/rfc5322#section-3.6.2"],
+    ["In-Reply-To", "https://tools.ietf.org/html/rfc5322#section-3.6.4"],
+    ["Importance", "https://tools.ietf.org/html/rfc2156#section-5.3"],
+    ["List-Help", "https://tools.ietf.org/html/rfc2369"],
+    ["List-ID", "https://tools.ietf.org/html/rfc2919"],
+    ["List-Subscribe", "https://tools.ietf.org/html/rfc2369"],
+    ["List-Unsubscribe", "https://tools.ietf.org/html/rfc2369"],
+    ["Message-ID", "https://tools.ietf.org/html/rfc5322#section-3.6.4"],
+    ["MIME-Version", "https://tools.ietf.org/html/rfc2045#section-4"],
+    ["Received", "https://tools.ietf.org/html/rfc5321#section-4.4"],
+    ["Received-SPF", "https://tools.ietf.org/html/rfc4408#section-7"],
+    ["References", "https://tools.ietf.org/html/rfc5322#section-3.6.4"],
+    ["Reply-To", "https://tools.ietf.org/html/rfc5322#section-3.6.2"],
+    ["Return-Path", "https://tools.ietf.org/html/rfc5322#section-3.6.7"],
+    ["Sender", "https://tools.ietf.org/html/rfc5322#section-3.6.2"],
+    ["Subject", "https://tools.ietf.org/html/rfc5322#section-3.6.5"],
+    ["Thread-Index", "https://msdn.microsoft.com/en-us/library/ms526219"],
+    ["Thread-Topic", "https://msdn.microsoft.com/en-us/library/ms526986"],
+    ["To", "https://tools.ietf.org/html/rfc5322#section-3.6.3"],
+    ["X-Auto-Response-Suppress", "https://msdn.microsoft.com/en-us/library/ee219609"],
+    ["X-Forefront-Antispam-Report", "https://technet.microsoft.com/en-us/library/dn205071"],
+    ["X-Forefront-Antispam-Report-Untrusted", "https://technet.microsoft.com/en-us/library/bb232136"],
+    ["X-Forefront-Prvs", "https://technet.microsoft.com/en-us/library/dd639361"],
+    ["X-Message-Flag", "https://msdn.microsoft.com/en-us/library/exchange/ms875195"],
+    ["X-Microsoft-Antispam", "https://technet.microsoft.com/en-us/library/dn205071"],
+    ["X-MS-Exchange-Organization-Antispam-Report", "https://technet.microsoft.com/en-us/library/aa996878"],
+    ["X-MS-Exchange-Organization-AuthAs", "https://technet.microsoft.com/en-us/library/bb232136"],
+    ["X-MS-Exchange-Organization-AuthMechanism", "https://technet.microsoft.com/en-us/library/bb232136"],
+    ["X-MS-Exchange-Organization-AuthSource", "https://technet.microsoft.com/en-us/library/bb232136"],
+    ["X-MS-Exchange-Organization-AutoForwarded", "https://msdn.microsoft.com/en-us/library/ee178180"],
+    ["X-MS-Exchange-Organization-AVStamp-Enterprise", "https://technet.microsoft.com/en-us/library/bb232136"],
+    ["X-MS-Exchange-Organization-AVStamp-Mailbox", "https://technet.microsoft.com/en-us/library/bb232136"],
+    ["X-MS-Exchange-Organization-Network-Message-Id", "https://technet.microsoft.com/en-us/library/bb232136"],
+    ["X-MS-Exchange-Organization-PCL", "https://technet.microsoft.com/en-us/library/aa996878"],
+    ["X-MS-Exchange-Organization-SCL", "https://technet.microsoft.com/en-us/library/aa996878"],
+    ["X-MS-Exchange-Organization-SenderIdResult", "https://technet.microsoft.com/en-us/library/aa996878"],
+    ["X-MS-Has-Attach", "https://msdn.microsoft.com/en-us/library/ee178420"],
+    ["X-MS-TNEF-Correlator", "https://msdn.microsoft.com/en-us/library/ee219198"],
+    ["X-Originating-IP", "https://en.wikipedia.org/wiki/X-Originating-IP"],
+    ["X-Priority", "https://technet.microsoft.com/en-us/library/bb691107"]
+];
