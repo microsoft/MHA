0939b8f25b153ab52d5a189049dcd7492fd1df6c
Stephen Griffin <sgriffin@outlook.com>
2019-12-13 18:10:00 -0500

    add build task

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,14 +1,21 @@
 const fs = require("fs");
 const path = require("path");
 
-const scriptsFolder = path.join(__dirname, "..", "Scripts");
-
-const files = fs.readdirSync(scriptsFolder);
-for (const file of files) {
-    console.log("Considering " + file);
-    if (file.match(/\.min\.js/)) {
-        console.log("  Deleting " + file);
-        const filePath = path.join(scriptsFolder, file);
-        fs.unlinkSync(filePath);
+function purge(files, sourcePath) {
+    for (const file of files) {
+        console.log("Considering " + file);
+        if (file.match(/aikey\.js/)) continue;
+        if (file.match(/\.js/)) {
+            console.log("  Deleting " + file);
+            const filePath = path.join(sourcePath, file);
+            fs.unlinkSync(filePath);
+        }
     }
 }
+
+const scriptsFolder = path.join(__dirname, "..", "Scripts");
+const files = fs.readdirSync(scriptsFolder);
+purge(files, scriptsFolder);
+const utFolder = path.join(__dirname, "..", "Scripts", "unittests");
+const ut = fs.readdirSync(utFolder);
+purge(ut, utFolder);
