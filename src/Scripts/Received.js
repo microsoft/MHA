0f1ec7ae5b53cf08cde4096f3c76cb4ee40e038d
Stephen Griffin <sgriffin@outlook.com>
2020-07-26 19:14:41 -0400

    rebuild recieved to make toString easier to implement

--- src/Scripts/Received.js
+++ src/Scripts/Received.js
@@ -11,129 +11,145 @@ var Received = (function () {
     // This algorithm should work regardless of the order of the headers, given:
     //  - The date, if present, is always at the end, separated by a ";".
     // Values not attached to a header will not be reflected in output.
-    function parseHeader(receivedHeader) {
-        var parsedRow = {
-            sourceHeader: receivedHeader,
-            delaySort: -1, // Force the "no previous or current time" rows to sort before the 0 second rows
-            percent: 0
-        }
-
-        if (!receivedHeader) { return parsedRow; }
-        // Strip linefeeds first
-        receivedHeader = receivedHeader.replace(/\r|\n|\r\n/g, ' ')
-
+    var parseHeader = function (receivedHeader) {
+        var sourceHeader = receivedHeader;
+        var date = "";
+        var dateNum = 0;
+        var dateSort = 0;
+        var fields = {};
         var receivedHeaderNames = ["from", "by", "with", "id", "for", "via"];
 
-        // Build array of header locations
-        var headerMatches = [];
+        if (receivedHeader) {
+            // Strip linefeeds first
+            receivedHeader = receivedHeader.replace(/\r|\n|\r\n/g, ' ')
 
-        // Some bad dates don't wrap UTC in paren - fix that first
-        receivedHeader = receivedHeader.replace(/UTC|\(UTC\)/gi, "(UTC)");
+            // Build array of header locations
+            var headerMatches = [];
 
-        // Read out the date first, then clear it from the string
-        var iDate = receivedHeader.lastIndexOf(";");
+            // Some bad dates don't wrap UTC in paren - fix that first
+            receivedHeader = receivedHeader.replace(/UTC|\(UTC\)/gi, "(UTC)");
 
-        // No semicolon means no date - or maybe there's one there?
-        // Sendgrid is bad about this
-        if (iDate === -1) {
-            // First try to find a day of the week
-            receivedHeader = receivedHeader.replace(/\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun)/g, "; $1");
-            iDate = receivedHeader.lastIndexOf(";");
-        }
+            // Read out the date first, then clear it from the string
+            var iDate = receivedHeader.lastIndexOf(";");
 
-        if (iDate === -1) {
-            // Next we look for year-month-day, 4-1/2-1/2
-            receivedHeader = receivedHeader.replace(/\s*(\d{4}-\d{1,2}-\d{1,2})/g, "; $1");
-            iDate = receivedHeader.lastIndexOf(";");
-        }
-
-        if (iDate !== -1 && receivedHeader.length !== iDate + 1) {
-            var date = receivedHeader.substring(iDate + 1);
-            receivedHeader = receivedHeader.substring(0, iDate);
-
-            // Invert any backwards dates: 2018-01-28 -> 01-28-2018
-            date = date.replace(/\s*(\d{4})-(\d{1,2})-(\d{1,2})/g, "$2/$3/$1");
-            // Replace dashes with slashes
-            date = date.replace(/\s*(\d{1,2})-(\d{1,2})-(\d{4})/g, "$1/$2/$3");
-
-            // If we don't have a +xxxx or -xxxx on our date, it will be interpreted in local time
-            // This likely isn't the intended timezone, so we add a +0000 to get UTC
-            var offset = date.match(/[+|-]\d{4}/);
-            if (!offset || offset.length !== 1) {
-                date += " +0000";
+            // No semicolon means no date - or maybe there's one there?
+            // Sendgrid is bad about this
+            if (iDate === -1) {
+                // First try to find a day of the week
+                receivedHeader = receivedHeader.replace(/\s*(Mon|Tue|Wed|Thu|Fri|Sat|Sun)/g, "; $1");
+                iDate = receivedHeader.lastIndexOf(";");
             }
 
-            // Some browsers don't like milliseconds in parse
-            // Trim off milliseconds so we don't pass them into Date.parse
-            var milliseconds = date.match(/\d{1,2}:\d{2}:\d{2}.(\d+)/);
-            date = date.replace(/(\d{1,2}:\d{2}:\d{2}).(\d+)/, "$1");
-
-            // And now we can parse our date
-            parsedRow.dateNum = Date.parse(date);
-            if (milliseconds && milliseconds.length >= 2) {
-                parsedRow.dateNum = parsedRow.dateNum + Math.floor(parseFloat("0." + milliseconds[1]) * 1000);
+            if (iDate === -1) {
+                // Next we look for year-month-day, 4-1/2-1/2
+                receivedHeader = receivedHeader.replace(/\s*(\d{4}-\d{1,2}-\d{1,2})/g, "; $1");
+                iDate = receivedHeader.lastIndexOf(";");
             }
 
-            parsedRow.date = dateString(date);
-            parsedRow.dateSort = parsedRow.dateNum;
-        }
+            if (iDate !== -1 && receivedHeader.length !== iDate + 1) {
+                date = receivedHeader.substring(iDate + 1);
+                receivedHeader = receivedHeader.substring(0, iDate);
 
-        // Scan for malformed postFix headers
-        // Received: by example.com (Postfix, from userid 1001)
-        //   id 1234ABCD; Thu, 21 Aug 2014 12:12:48 +0200 (CEST)
-        var postFix = receivedHeader.match(/(.*)by (.*? \(Postfix, from userid .*?\))(.*)/mi);
-        if (postFix) {
-            parsedRow.by = postFix[2];
-            receivedHeader = postFix[1] + postFix[3];
-            receivedHeaderNames = removeEntry(receivedHeaderNames, "by");
-        }
+                // Invert any backwards dates: 2018-01-28 -> 01-28-2018
+                date = date.replace(/\s*(\d{4})-(\d{1,2})-(\d{1,2})/g, "$2/$3/$1");
+                // Replace dashes with slashes
+                date = date.replace(/\s*(\d{1,2})-(\d{1,2})-(\d{4})/g, "$1/$2/$3");
 
-        // Scan for malformed qmail headers
-        // Received: (qmail 10876 invoked from network); 24 Aug 2014 16:13:38 -0000
-        var qmail = receivedHeader.match(/(.*)\((qmail .*? invoked from .*?)\)(.*)/mi);
-        if (qmail) {
-            parsedRow.by = qmail[2];
-            receivedHeader = qmail[1] + qmail[3];
-            receivedHeaderNames = removeEntry(receivedHeaderNames, "by");
-        }
-
-        // Split up the string now so we can look for our headers
-        var tokens = receivedHeader.split(/\s+/);
-
-        var iMatch = 0;
-        receivedHeaderNames.forEach(function (receivedHeaderName, iHeader) {
-            tokens.some(function (token, iToken) {
-                if (receivedHeaderName.toUpperCase() === token.toUpperCase()) {
-                    headerMatches[iMatch++] = { iHeader: iHeader, iToken: iToken };
-                    // We don't return true so we can match any duplicate headers
-                    // In doing this, we risk failing to parse a string where a header
-                    // keyword appears as the value for another header
-                    // Both situations are invalid input
-                    // We're just picking which one we'd prefer to handle
+                // If we don't have a +xxxx or -xxxx on our date, it will be interpreted in local time
+                // This likely isn't the intended timezone, so we add a +0000 to get UTC
+                var offset = date.match(/[+|-]\d{4}/);
+                if (!offset || offset.length !== 1) {
+                    date += " +0000";
                 }
-            });
-        });
 
-        // Next bit assumes headerMatches[x,y] is increasing on y.
-        // Sort it so it is.
-        headerMatches.sort(function (a, b) { return a.iToken - b.iToken; });
+                // Some browsers don't like milliseconds in parse
+                // Trim off milliseconds so we don't pass them into Date.parse
+                var milliseconds = date.match(/\d{1,2}:\d{2}:\d{2}.(\d+)/);
+                date = date.replace(/(\d{1,2}:\d{2}:\d{2}).(\d+)/, "$1");
 
-        headerMatches.forEach(function (headerMatch, iMatch) {
-            var iNextTokenHeader;
-            if (iMatch + 1 < headerMatches.length) {
-                iNextTokenHeader = headerMatches[iMatch + 1].iToken;
-            } else {
-                iNextTokenHeader = tokens.length;
+                // And now we can parse our date
+                dateNum = Date.parse(date);
+                if (milliseconds && milliseconds.length >= 2) {
+                    dateNum = dateNum + Math.floor(parseFloat("0." + milliseconds[1]) * 1000);
+                }
+
+                date = dateString(date);
+                dateSort = dateNum;
             }
 
-            var headerName = receivedHeaderNames[headerMatch.iHeader];
-            if (parsedRow[headerName] === undefined) { parsedRow[headerName] = ""; }
-            if (parsedRow[headerName] !== "") { parsedRow[headerName] += "; "; }
-            parsedRow[headerName] += tokens.slice(headerMatch.iToken + 1, iNextTokenHeader).join(" ").trim();
+            // Scan for malformed postFix headers
+            // Received: by example.com (Postfix, from userid 1001)
+            //   id 1234ABCD; Thu, 21 Aug 2014 12:12:48 +0200 (CEST)
+            var postFix = receivedHeader.match(/(.*)by (.*? \(Postfix, from userid .*?\))(.*)/mi);
+            if (postFix) {
+                fields["by"] = postFix[2];
+                receivedHeader = postFix[1] + postFix[3];
+                receivedHeaderNames = removeEntry(receivedHeaderNames, "by");
+            }
+
+            // Scan for malformed qmail headers
+            // Received: (qmail 10876 invoked from network); 24 Aug 2014 16:13:38 -0000
+            var qmail = receivedHeader.match(/(.*)\((qmail .*? invoked from .*?)\)(.*)/mi);
+            if (qmail) {
+                fields["by"] = qmail[2];
+                receivedHeader = qmail[1] + qmail[3];
+                receivedHeaderNames = removeEntry(receivedHeaderNames, "by");
+            }
+
+            // Split up the string now so we can look for our headers
+            var tokens = receivedHeader.split(/\s+/);
+
+            var iMatch = 0;
+            receivedHeaderNames.forEach(function (receivedHeaderName, iHeader) {
+                tokens.some(function (token, iToken) {
+                    if (receivedHeaderName.toUpperCase() === token.toUpperCase()) {
+                        headerMatches[iMatch++] = { iHeader: iHeader, iToken: iToken };
+                        // We don't return true so we can match any duplicate headers
+                        // In doing this, we risk failing to parse a string where a header
+                        // keyword appears as the value for another header
+                        // Both situations are invalid input
+                        // We're just picking which one we'd prefer to handle
+                    }
+                });
+            });
+
+            // Next bit assumes headerMatches[x,y] is increasing on y.
+            // Sort it so it is.
+            headerMatches.sort(function (a, b) { return a.iToken - b.iToken; });
+
+            headerMatches.forEach(function (headerMatch, iMatch) {
+                var iNextTokenHeader;
+                if (iMatch + 1 < headerMatches.length) {
+                    iNextTokenHeader = headerMatches[iMatch + 1].iToken;
+                } else {
+                    iNextTokenHeader = tokens.length;
+                }
+
+                var headerName = receivedHeaderNames[headerMatch.iHeader];
+                if (fields[headerName] === undefined) { fields[headerName] = ""; }
+                if (fields[headerName] !== "") { fields[headerName] += "; "; }
+                fields[headerName] += tokens.slice(headerMatch.iToken + 1, iNextTokenHeader).join(" ").trim();
+            });
+        }
+
+        var ret = {
+            sourceHeader: sourceHeader,
+            delaySort: -1, // Force the "no previous or current time" rows to sort before the 0 second rows
+            percent: 0,
+        };
+
+        if (date) ret.date = date;
+        if (dateNum || isNaN(dateNum)) ret.dateNum = dateNum;
+        if (dateSort || isNaN(dateSort)) ret.dateSort = dateSort;
+        if (fields["by"]) ret["by"] = fields["by"];
+
+        // Add parsed fields to the row before returning
+        receivedHeaderNames.forEach(function (receivedHeaderName) {
+            if (fields[receivedHeaderName]) ret[receivedHeaderName] = fields[receivedHeaderName];
         });
 
-        return parsedRow;
-    }
+        return ret;
+    };
 
     function exists() { return receivedRows.length > 0; }
 
@@ -283,6 +299,13 @@ var Received = (function () {
         parseHeader: parseHeader, // For testing only
         dateString: dateString, // For testing only
         computeTime: computeTime, // For testing only
-        toString: function () { return "received"; }
+        toString: function () {
+            if (!exists()) return "";
+            var ret = ["Received"];
+            receivedRows.forEach(function (row) {
+                ret.push(row);
+            })
+            return ret.join("\n");
+        }
     };
 });
