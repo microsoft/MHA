b9849f15c68d71ad39847b3e784ace727e0b5370
Stephen Griffin <sgriffin@outlook.com>
2020-09-19 21:21:33 -0400

    update deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -115,7 +115,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 echo 4. Transpile TypeScript
 IF EXIST "%DEPLOYMENT_TARGET%\tsconfig.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd tsc
+  call :ExecuteCmd node %DEPLOYMENT_TARGET%\node_modules\typescript\bin\tsc -p "%DEPLOYMENT_TARGET%"
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
