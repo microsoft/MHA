6e35088963866165726f14575ffbb6d1b4e2b92d
Stephen Griffin <sgriffin@outlook.com>
2020-10-07 20:37:18 -0400

    move button logic into standalone

--- src/Scripts/StandAlone.ts
+++ src/Scripts/StandAlone.ts
@@ -11,20 +11,30 @@ var StandAlone = (function () {
 
     var viewModel = null;
 
-    if (window.jQuery) {
-        $(document).ready(function () {
-            Diagnostics.set("API used", "standalone");
-            viewModel = HeaderModel();
-            Table.initializeTableUI(viewModel);
-            Table.makeResizablePane("inputHeaders", mhaStrings.mha_prompt, null);
-        });
+    function enableSpinner() {
+        $("#response").css("background-image", "url(/Resources/loader.gif)");
+        $("#response").css("background-repeat", "no-repeat");
+        $("#response").css("background-position", "center");
+    }
+
+    function disableSpinner() {
+        $("#response").css("background", "none");
+    }
+
+    function updateStatus(statusText) {
+        $("#status").text(statusText);
+        if (viewModel !== null) {
+            viewModel.status = statusText;
+        }
+
+        Table.recalculateVisibility();
     }
 
     // Do our best at recognizing RFC 2822 headers:
     // http://tools.ietf.org/html/rfc2822
     function analyze() {
         // Can't do anything without jQuery
-        if (!window.jQuery) return;
+        if (!$) return;
         if (appInsights) appInsights.trackEvent("analyzeHeaders");
         viewModel = HeaderModel($("#inputHeaders").val());
         Table.resetArrows();
@@ -46,32 +56,22 @@ var StandAlone = (function () {
         Table.rebuildSections(viewModel);
     }
 
-    function enableSpinner() {
-        $("#response").css("background-image", "url(/Resources/loader.gif)");
-        $("#response").css("background-repeat", "no-repeat");
-        $("#response").css("background-position", "center");
-    }
-
-    function disableSpinner() {
-        $("#response").css("background", "none");
-    }
-
-    function updateStatus(statusText) {
-        $("#status").text(statusText);
-        if (viewModel !== null) {
-            viewModel.status = statusText;
-        }
-
-        Table.recalculateVisibility();
-    }
-
     function copy() {
         mhaStrings.copyToClipboard(viewModel.toString());
     }
 
-    return {
-        analyze: analyze,
-        clear: clear,
-        copy: copy
-    };
+    if ($) {
+        $(document).ready(function () {
+            Diagnostics.set("API used", "standalone");
+            viewModel = HeaderModel();
+            Table.initializeTableUI(viewModel);
+            Table.makeResizablePane("inputHeaders", mhaStrings.mha_prompt, null);
+
+            document.querySelector("#analyzeButton").onclick = analyze;
+            document.querySelector("#clearButton").onclick = clear;
+            document.querySelector("#copyButton").onclick = copy;
+        });
+    }
+
+    return {};
 })();
