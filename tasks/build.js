d6737c862c29cf43f311c5e70448289fd9261d47
Stephen Griffin <stephenegriffin@users.noreply.github.com>
2022-05-18 08:43:58 -0400

    Update build.js

--- tasks/build.js
+++ tasks/build.js
@@ -33,6 +33,7 @@ const getHash = function (str) {
 };
 
 var commitID = process.env.SCM_COMMIT_ID;
+if (!commitID) commitID = process.env.github.sha;
 if (!commitID) commitID = "test";
 const version = getHash(commitID);
 console.log("commitID: " + commitID);
