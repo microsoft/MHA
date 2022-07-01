d4555666bb0eb3f6427af04730f47789b5dee685
Stephen Griffin <sgriffin@outlook.com>
2022-06-30 23:23:06 -0400

    undo

--- src/Scripts/Headers.ts
+++ src/Scripts/Headers.ts
@@ -7,9 +7,9 @@ import { Summary } from "./Summary"
 import { poster } from "./poster"
 
 export class Header {
-    constructor(header: string, value: string | null) {
+    constructor(header: string, value: string) {
         this.header = header;
-        this.value = value ?? "";
+        this.value = value;
     }
     header: string;
     value: string;
