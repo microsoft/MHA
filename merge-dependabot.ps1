d2cc8840df2098f20f793a30286b0f035f30b391
Stephen Griffin <stephenegriffin@users.noreply.github.com>
2023-05-25 09:21:25 -0400

    U/sgriffin/dependabot merge (#573)
    * Add merge script
    
    * update script
    
    * remove user name from merge script

---
+++ merge-dependabot.ps1
@@ -0,0 +1,22 @@
+﻿# Get back on main with no changes
+git merge --abort
+git checkout main
+git pull
+
+# Checkout a new branch to merge the Dependabot branches into
+git checkout -b dependabot-merge
+git checkout dependabot-merge
+
+# Fetch the latest branch information from the remote repository
+git fetch
+
+# Retrieve the list of branches starting with 'origin/dependabot'
+$dependabotBranches = git branch -r --list 'origin/dependabot/*' | ForEach-Object { $_ -replace '^.*origin/', '' }
+
+# Merge the Dependabot branches one by one
+foreach ($branch in $dependabotBranches) {
+    git merge --no-ff "origin/$branch" -m "Merge origin/$branch into u/sgriffin/dependabot-merge"
+}
+
+# Push the merged branch to the remote repository
+git push origin dependabot-merge
