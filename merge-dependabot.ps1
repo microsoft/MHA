f08e9eb7d7132371473895dec2a622305be5ef5b
Stephen Griffin <sgriffin@outlook.com>
2023-05-25 09:08:32 -0400

    Add merge script

---
+++ merge-dependabot.ps1
@@ -0,0 +1,21 @@
+﻿# Get back on main with no changes
+git merge --abort
+git checkout main
+git pull
+
+# Checkout a new branch to merge the Dependabot branches into
+git checkout -b u/sgriffin/dependabot-merge
+
+# Fetch the latest branch information from the remote repository
+git fetch
+
+# Retrieve the list of branches starting with 'origin/dependabot'
+git branch -r --list 'origin/dependabot/*' | ForEach-Object { $_ -replace '^.*origin/', '' }
+
+# Merge the Dependabot branches one by one
+foreach ($branch in $dependabotBranches) {
+    git merge --no-ff "origin/$branch" -m "Merge origin/$branch into u/sgriffin/dependabot-merge"
+}
+
+# Push the merged branch to the remote repository
+#git push origin u/sgriffin/dependabot-merge
