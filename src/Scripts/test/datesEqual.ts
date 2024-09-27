68250842b60aafda41bbf25dcae26f2b0e8867f5
Stephen Griffin <sgriffin@outlook.com>
2024-09-27 18:25:21 -0400

    add datetime tests

---
+++ src/Scripts/test/datesEqual.ts
@@ -0,0 +1,42 @@
+import type { MatcherFunction } from "expect";
+import { ReceivedRow } from "../Received";
+import { expect } from "@jest/globals";
+
+export const datesEqual: MatcherFunction<[expected: unknown]> =
+    function (_actual: unknown, _expected: unknown) {
+        const actual = _actual as ReceivedRow;
+        const expected = _expected as { [index: string]: any };
+        let passed = true;
+        const messages: string[] = [];
+
+        if (actual.date === undefined) {
+            passed = false;
+            messages.push("date is undefined");
+        } else {
+            const date = new Date(actual.date.value);
+            const dateStr = date.toLocaleString("en-US", { timeZone: "America/New_York" });
+            if (dateStr !== expected["date"]) {
+                passed = false;
+                messages.push(`date: ${dateStr} !== ${expected["date"]}`);
+            }
+
+            const dateNum = actual.dateNum.toString();
+            if (dateNum !== expected["dateNum"]) {
+                passed = false;
+                messages.push(`dateNum: ${dateNum} !== ${expected["dateNum"]}`);
+            }
+        }
+
+        return {
+            pass: passed,
+            message: () => messages.join("; "),
+        };
+    };
+
+expect.extend({ datesEqual, });
+
+declare module "expect" {
+    interface Matchers<R> {
+        datesEqual(expected: { [index: string]: any }): R;
+    }
+}
