ca520d4a49f269e243717cb9543f4bf0db9aadad
Stephen Griffin <sgriffin@outlook.com>
2024-10-01 17:29:00 -0400

    remove more any

--- src/Scripts/poster.ts
+++ src/Scripts/poster.ts
@@ -1,13 +1,13 @@
 export class poster {
     public static site() { return window.location.protocol + "//" + window.location.host; }
 
-    public static postMessageToFrame(frame: Window, eventName: string, data?: any): void {
+    public static postMessageToFrame(frame: Window, eventName: string, data?: unknown): void {
         if (frame) {
             frame.postMessage({ eventName: eventName, data: data }, poster.site());
         }
     }
 
-    public static postMessageToParent(eventName: string, data?: any): void {
+    public static postMessageToParent(eventName: string, data?: unknown): void {
         window.parent.postMessage({ eventName: eventName, data: data }, poster.site());
     }
 }
