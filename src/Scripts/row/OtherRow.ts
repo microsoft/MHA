42077efec82a510b3f8a34ed216f01ecc30b509f
Stephen Griffin <sgriffin@outlook.com>
2024-10-02 12:49:33 -0400

    remove another any

--- src/Scripts/row/OtherRow.ts
+++ src/Scripts/row/OtherRow.ts
@@ -2,7 +2,7 @@ import { strings } from "../Strings";
 import { Row } from "./Row";
 
 export class OtherRow extends Row {
-    constructor(number: number, header: string, value: any) {
+    constructor(number: number, header: string, value: string) {
         super(header, "", "");
         this.number = number;
         this.value = value;
