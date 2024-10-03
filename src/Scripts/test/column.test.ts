4e48e10ffbf9bd3b855d84762ea97bcc8ddfefbd
Stephen Griffin <sgriffin@outlook.com>
2024-10-02 21:08:57 -0400

    add some tests

---
+++ src/Scripts/test/column.test.ts
@@ -0,0 +1,29 @@
+import { column } from "../table/column";
+
+describe("column", () => {
+    it("should create an instance with the given id, label, and class", () => {
+        const id = "col1";
+        const label = "Column 1";
+        const columnClass = "class1";
+        const col = new column(id, label, columnClass);
+
+        expect(col.id).toBe(id);
+        expect(col.label).toBe(label);
+        expect(col.class).toBe(columnClass);
+    });
+
+    it("should have id as a string", () => {
+        const col = new column("col2", "Column 2", "class2");
+        expect(typeof col.id).toBe("string");
+    });
+
+    it("should have label as a string", () => {
+        const col = new column("col3", "Column 3", "class3");
+        expect(typeof col.label).toBe("string");
+    });
+
+    it("should have class as a string", () => {
+        const col = new column("col4", "Column 4", "class4");
+        expect(typeof col.class).toBe("string");
+    });
+});
