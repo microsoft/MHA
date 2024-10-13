199dd91346b64c471396c4d0ac96aa1999c9558a
Stephen Griffin <sgriffin@outlook.com>
2024-10-13 19:05:43 -0400

    add tests to match

---
+++ src/Scripts/row/match.test.ts
@@ -0,0 +1,26 @@
+import { Match } from "./match";
+
+describe("Match", () => {
+    it("should create an instance with the given fieldName and iToken", () => {
+        const fieldName = "testField";
+        const iToken = 123;
+        const match = new Match(fieldName, iToken);
+
+        expect(match.fieldName).toBe(fieldName);
+        expect(match.iToken).toBe(iToken);
+    });
+
+    it("should have readonly properties", () => {
+        const match = new Match("testField", 123);
+
+        expect(() => {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            (match as any).fieldName = "newField";
+        }).toThrow();
+
+        expect(() => {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            (match as any).iToken = 456;
+        }).toThrow();
+    });
+});
--- src/Scripts/row/match.ts
+++ src/Scripts/row/match.ts
@@ -1,3 +1,13 @@
 export class Match {
-    fieldName = ""; iToken = 0;
+    public readonly fieldName: string;
+    public readonly iToken: number;
+
+    constructor(fieldName: string, iToken: number) {
+        this.fieldName = fieldName;
+        this.iToken = iToken;
+
+        // Make properties non-writable
+        Object.defineProperty(this, "fieldName", { writable: false });
+        Object.defineProperty(this, "iToken", { writable: false });
+    }
 }
