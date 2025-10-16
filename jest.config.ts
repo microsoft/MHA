9a79f4418c97c4235d8ddc7bb1e3f3c73608afc7
Stephen Griffin <sgriffin@microsoft.com>
2025-10-16 11:29:31 -0400

    Update build configs to match main branch
    - Updated package.json with main's dependencies (added Jest, removed jQuery types)
    - Added jest.config.ts from main branch
    - Updated tsconfig.json to main's stricter TypeScript settings
    - Temporarily added jQuery to fix build until Scripts are modernized
    - Build works successfully

---
+++ jest.config.ts
@@ -0,0 +1,33 @@
+import type {Config} from "jest";
+// https://github.com/jest-community/awesome-jest
+const config: Config = {
+    testEnvironment: "jsdom",
+    globalSetup: "./global-setup.js",
+    transform: {
+        "^.+.tsx?$": ["ts-jest",{ diagnostics: { ignoreCodes: ["TS151001"] } }],
+    },
+    globals: {
+        "__AIKEY__": ""
+    },
+    collectCoverage: true,
+    collectCoverageFrom: ["./src/**"],
+    coverageDirectory: "./Pages/coverage",
+    coverageReporters: ["json", "lcov", "text", "clover", "text-summary"],
+    coverageThreshold: {
+        global: {
+            branches: 35,
+            functions: 40,
+            lines: 40,
+            statements: 40,
+        },
+    },
+    reporters: [
+        "default",
+        ["jest-html-reporters", {
+            "publicPath": "./Pages/test",
+            "filename": "index.html"
+        }]
+    ]
+};
+
+export default config;
